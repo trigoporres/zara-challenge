b9e8162d2e18c52cc021a8191c897feec8f78785
Santiago Trigo Porres <santiagotrigoporres@gmail.com>
2026-01-23 10:04:29 +0100

    feat: implement filterProductsById utility function and corresponding tests

---
+++ src/hooks/utils/filterProductsById.test.ts
@@ -0,0 +1,74 @@
+import { describe, expect } from 'vitest';
+import { filterProductsById } from './filterProductsById';
+import type { Product } from '../useProducts';
+
+describe('filterProductsById', () => {
+  const cases: [string, Product[], Product[]][] = [
+    [
+      'without duplicates should return same array',
+      [
+        {
+          basePrice: 100,
+          brand: 'BrandA',
+          id: '1',
+          imageUrl: 'url1',
+          name: 'Product1',
+        },
+        {
+          basePrice: 200,
+          brand: 'BrandB',
+          id: '2',
+          imageUrl: 'url2',
+          name: 'Product2',
+        },
+      ],
+      [
+        {
+          basePrice: 100,
+          brand: 'BrandA',
+          id: '1',
+          imageUrl: 'url1',
+          name: 'Product1',
+        },
+        {
+          basePrice: 200,
+          brand: 'BrandB',
+          id: '2',
+          imageUrl: 'url2',
+          name: 'Product2',
+        },
+      ],
+    ],
+    [
+      'whit duplicates should return filtered array',
+      [
+        {
+          basePrice: 100,
+          brand: 'BrandA',
+          id: '1',
+          imageUrl: 'url1',
+          name: 'Product1',
+        },
+        {
+          basePrice: 200,
+          brand: 'BrandA',
+          id: '1',
+          imageUrl: 'url1',
+          name: 'Product1',
+        },
+      ],
+      [
+        {
+          basePrice: 100,
+          brand: 'BrandA',
+          id: '1',
+          imageUrl: 'url1',
+          name: 'Product1',
+        },
+      ],
+    ],
+  ];
+  test.each(cases)('%s', (_title, products, expected) => {
+    expect(filterProductsById(products)).toStrictEqual(expected);
+  });
+});
---
+++ src/hooks/utils/filterProductsById.ts
@@ -0,0 +1,11 @@
+import type { Product } from '../useProducts'
+
+export const filterProductsById = (products: Product[]) => {
+  const uniqueProductsMap = new Map<string, Product>()
+  products.forEach(product => {
+    if (!uniqueProductsMap.has(product.id)) {
+      uniqueProductsMap.set(product.id, product)
+    }
+  })
+  return Array.from(uniqueProductsMap.values())
+}
