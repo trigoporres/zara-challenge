2d64d39b6555e54c85e13dcd533fe9b63aee7a38
Santiago Trigo Porres <santiagotrigoporres@gmail.com>
2026-01-24 22:42:57 +0100

    feat: restructure App component to include CartProvider and AppHeader; update routing for product details and cart views

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './views'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './views';
+import { Details } from './views/Details';
+import { CartProvider } from './contexts/CartContext';
+import { Cart } from './views/Cart';
+import { AppHeader } from './components/header/AppHeader';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<div>Página de detalles</div>} />
-      </Routes>
+      <CartProvider>
+        <AppHeader />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Home />} />
+          <Route path="/product/:id" element={<Details />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </CartProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
